56d1896d57c4375b71d595d19223df15d101d566
nordluma <maxnordlund.95@hotmail.com>
2023-05-31 18:31:55 +0300

    feat: add route to get user by id

--- src/server/api/routers/stations.ts
+++ src/server/api/routers/stations.ts
@@ -1,12 +1,34 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const stationRouter = createTRPCRouter({
     getAll: publicProcedure.query(async ({ ctx }) => {
         const stations = await ctx.prisma.stations.findMany({
+            select: {
+                id: true,
+                name_fin: true,
+            },
             orderBy: {
                 name_fin: "asc"
             }
         });
         return stations;
     }),
+    getById: publicProcedure
+        .input(z.object({ id: z.number() }))
+        .query(async ({ ctx, input }) => {
+            const station = await ctx.prisma.stations.findUnique({
+                where: { id: input.id }
+            });
+
+            if (!station) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Station not found",
+                })
+            }
+
+            return station
+        })
 });
